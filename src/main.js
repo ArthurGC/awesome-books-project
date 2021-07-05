ecf780a61f3100672e35f67c059720845a4a2eeb
arthurGC <aguzmanc@unsa.edu.pe>
2021-07-05 10:54:46 -0500

    Add getBookData function

--- src/main.js
+++ src/main.js
@@ -2,3 +2,14 @@ import { booksList } from "./books.js";
 
 const form = document.querySelector("#form");
 const bookContainer = document.querySelector(".container");
+
+const getBookData = () => {
+    let books;
+    if (localStorage.getItem("BooksData") != null) {
+      books = JSON.parse(localStorage.getItem("BooksData"));
+    } else {
+      books = booksList;
+    }
+  
+    return books;
+  };
