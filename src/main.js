50fc45dd502342840a7407be4f02e63c365f8d2c
arthurGC <aguzmanc@unsa.edu.pe>
2021-07-05 10:55:24 -0500

    Add addBookToViewport function

--- src/main.js
+++ src/main.js
@@ -13,3 +13,38 @@ const getBookData = () => {
   
     return books;
   };
+
+// const setBook = (book) => {
+//   const books = getBookData();
+//   books.push(book);
+//   localStorage.setItem("BooksData", JSON.stringify(books));
+// };
+
+// const removeBook = (element) => {
+//   const books = getBookData();
+//   let position =
+//     Array.prototype.indexOf.call(
+//       bookContainer.childNodes,
+//       element.parentElement
+//     ) - 1;
+
+//   if (element.classList.contains("btn")) {
+//     books.forEach((book, index) => {
+//       if (position == index) {
+//         books.splice(index, 1);
+//       }
+//       localStorage.setItem("BooksData", JSON.stringify(books));
+//     });
+//   }
+// };
+
+const addBookToViewport = (book) => {
+    const item = document.createElement("li");
+  
+    item.innerHTML = `
+          <p>${book.title} by ${book.author}</p>
+          <button class="btn">Remove</button>
+          `;
+  
+    bookContainer.appendChild(item);
+  };
